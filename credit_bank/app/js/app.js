ca7c520c225a8e302bdc705adcc287206e2b0fac
litvinovl-digital <70130791+litvinovl-digital@users.noreply.github.com>
2021-03-22 00:59:48 +0300

    добавил форму

--- credit_bank/app/js/app.js
+++ credit_bank/app/js/app.js
@@ -5,10 +5,55 @@
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
+require('~/app/libs/hystModal/dist/hystmodal.min.js')
 
+// import Vue from 'vue';
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+	let app = new Vue({
+		el: '#appVue',
+		data: {
+			errors: [],
+			name: null,
+			email: null,
+			tel: null,
+			nationality: null,
+			classValid: '',
+			
+		},
+		methods: {
+			checkForm: function (e) {
+			
+				this.errors = [];
+	
+				if (!this.name) {
+					this.errors.push('Требуется указать имя.');
+					this.classValid = 'invalid';
+				}
+				if (!this.email) {
+					this.errors.push('Требуется указать электронную почту.');
+				}
+				else if (!this.validEmail(this.email)) {
+					this.errors.push('Укажите корректный адрес электронной почты.');
+				}
+
+				if (!this.errors.length) {
+					// this.classValid = 'valid';
+					return true;
+					
+				}
+	
+				e.preventDefault();
+			},
+			validEmail: function (email) {
+				var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+				// this.classValid = 'valid';
+				return re.test(email);
+			}
+		}
+	});
 
 let tabs = document.querySelector('.tabs')
 let btns = tabs.querySelectorAll('.tabs__btn')
@@ -26,5 +71,11 @@ for(let i = 0; i < btns.length; i++) {
 		change([btns, items], i)
 	})
 }
+
+const myModal = new HystModal({
+	linkAttributeName: "data-hystmodal",
+	waitTransitions: true,
+	
+});
 
 })
